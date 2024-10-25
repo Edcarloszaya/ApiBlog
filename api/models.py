84a06aecf605288fda1380270c1ba73182bdc4c0
Edcarloszaya <edcarlos.molequedoido@gmail.com>
2024-10-25 08:01:32 -0300

    modelo do post no banco de dados

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# modelo do banco de dados 
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    category = models.CharField(max_length=200)
+    tags = models.JSONField(default=list)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
